6c7bb412b7a8f4a5e872feb2f5339c0b9d087f26
unknown <mattb.1099@gmail.com>
2022-11-01 20:35:29 -0500

    This is with the images included.

--- static/config.js
+++ static/config.js
@@ -1,4 +1,4 @@
 // For the API key
 
-const API_KEY = "";
+const API_KEY = "pk.eyJ1Ijoia2xvdWQ0MDQiLCJhIjoiY2w2Nnh1bm11MDBvYTNxazYxNzl0ZWJqNyJ9.kssHmHqi9XIWDvtupcALPQ";
 // Forgot to add a get ignore file so I can't leave my API key in here.
